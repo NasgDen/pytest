42f1d4753f6bdf655395ee155f6287a655ba9adb
Denis Safonov <denissafonov_85@mail.ru>
2025-06-21 01:09:32 +0500

    Добавлена функция calculate_tax с помощью TDD

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from src.utils import calculate_taxes
+from src.utils import calculate_tax, calculate_taxes
 
 
 def main():
     print(calculate_taxes([2, 4, 6], 10))
+    print(calculate_tax(20, 10000))
 
 
 if __name__ == "__main__":
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.utils import calculate_taxes
+from src.utils import calculate_tax, calculate_taxes
 
 
 @pytest.mark.parametrize("prices, tax_rate, expected", [
@@ -27,3 +27,24 @@ def test_calculate_price_raise():
         calculate_taxes([-1, -2], 10)
     # Проверяем, что сообщение об ошибке соответствует ожидаемому
     assert str(exc_info.value) == 'Неверная цена'
+
+
+# Тест calculate_tax
+def test_calculate_tax_price_raise():
+    with pytest.raises(ValueError) as exc_info:
+        calculate_tax(-10, 10)
+    assert str(exc_info.value) == "Неверная цена"
+
+
+def test_calculate_tax_tax_raise():
+    with pytest.raises(ValueError) as exc_info:
+        calculate_tax(10, -10)
+    assert str(exc_info.value) == "Неверный налоговый процент"
+
+
+@pytest.mark.parametrize("prices, tax_rate, expected", [
+    (100, 10, 110),
+    (50, 5, 52.5)
+])
+def test_calculate_tax_param(prices, tax_rate, expected):
+    assert calculate_tax(prices, tax_rate) == expected
